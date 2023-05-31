03600ff09e1257c7681b8f8d621a83d2ff7fde0d
maxwellmezadre <maxwellmezadre@gmail.com>
2023-05-31 14:36:33 -0300

    CORE-5: Adicionada as propriedades do model de clientes.

--- src/Models/Customer.php
+++ src/Models/Customer.php
@@ -4,6 +4,17 @@ namespace SamiSistemas\SamiERPCore\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property string $HostingSami IP do espelhamento
+ * @property string $Porta Porta do espelhamento
+ * @property string $usuarioMySQLSami Usuário do espelhamento
+ * @property string $senhaMySQLSami Senha do espelhamento
+ * @property string $HostingCliente IP do cliente
+ * @property string $PortaCliente Porta do cliente
+ * @property string $UsuarioMysqlCliente Usuário do cliente
+ * @property string $SenhaMySQLCliente Senha do cliente
+ * @property string $NomeBancoDados Nome do banco de dados
+ */
 class Customer extends Model
 {
     protected $connection = 'institutional';
